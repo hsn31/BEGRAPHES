acb741f703fae864a606cdbba7895721f510749c
NyuB <decaeste@etud.insa-toulouse.fr>
2019-04-18 09:32:43 +0200

    -Dijkstra tests implementation

--- src/main/org/insa/algo/shortestpath/AStarAlgorithm.java
+++ src/main/org/insa/algo/shortestpath/AStarAlgorithm.java
@@ -1,9 +1,16 @@
 package org.insa.algo.shortestpath;
 
+import org.insa.graph.Arc;
+import org.insa.graph.Point;
+
+
 public class AStarAlgorithm extends DijkstraAlgorithm {
 
     public AStarAlgorithm(ShortestPathData data) {
         super(data);
     }
-
+    @Override
+    protected double heuristic(Label from, Label to, Arc arc) {
+        return super.heuristic(from, to, arc)+ Point.distance(from.getNode().getPoint(),to.getNode().getPoint());
+    }
 }
