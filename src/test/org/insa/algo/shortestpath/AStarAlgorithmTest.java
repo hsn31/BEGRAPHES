8aa50fe3264f12360c86cf24a5200fa924f57b2c
NyuB <decaeste@etud.insa-toulouse.fr>
2019-05-06 15:10:09 +0200

    Ignored non-relevant case (simple graph) for euclidian heuristic

--- src/test/org/insa/algo/shortestpath/AStarAlgorithmTest.java
+++ src/test/org/insa/algo/shortestpath/AStarAlgorithmTest.java
@@ -1,8 +1,15 @@
 package org.insa.algo.shortestpath;
 
+import org.junit.Assume;
+
 public class AStarAlgorithmTest extends ShortestPathAlgorithmTest {
 	@Override
 	public ShortestPathAlgorithm instanciateAlgorithm(ShortestPathData shortestPathData) {
 		return new AStarAlgorithm(shortestPathData);
+	}
+
+	@Override
+	public void simpleGraphWithOraclePathTest(int from, int to) {
+		Assume.assumeTrue("Test graph not relevant for AStar Algorithm\n(distance between nodes can be inferior to their 'geographic' distance), thus the heuristic is an overestimation",false);
 	}
 }
