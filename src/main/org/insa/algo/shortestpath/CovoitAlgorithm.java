db3a4ce32d1bb3b25d684c9984dcd8a2ee6b0902
M. Decaestecker Brice <decaeste@insa-08132>
2019-05-14 18:21:15 +0200

    nit covoitAlgo

--- src/main/org/insa/algo/shortestpath/CovoitAlgorithm.java
+++ src/main/org/insa/algo/shortestpath/CovoitAlgorithm.java
@@ -1,5 +1,42 @@
 package org.insa.algo.shortestpath;
 
-public class CovoitAlgorithm {
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.insa.algo.utils.BinaryHeap;
+import org.insa.exception.NodeOutOfGraphException;
+import org.insa.graph.Graph;
+import org.insa.graph.Node;
+import org.insa.graph.Path;
+
+public class CovoitAlgorithm extends ShortestPathAlgorithm{
+	public CovoitAlgorithm(ShortestPathData data) throws NodeOutOfGraphException {
+		super(data);
+		
+	}
+	@Override
+	public ShortestPathSolution doRun() {
+		
+		ShortestPathSolution res;
+		BinaryHeap<Label> binaryHeap=new BinaryHeap<>();
+		Path solutionPath;
+		ArrayList<Node> nodePath = new ArrayList<>();
+		Graph graph = data.getGraph();
+		HashMap<Node, Label> labels = new HashMap<>();
+		
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		return null;
+		
+	}
 
 }
