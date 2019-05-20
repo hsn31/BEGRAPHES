c8aa92ee9e3fd70f6032b71265aabf171f587c7f
NyuB <decaeste@etud.insa-toulouse.fr>
2019-05-20 13:09:39 +0200

    Simple graph is now a relevant test(all nodes at (0,0))

--- src/test/org/insa/algo/shortestpath/AStarAlgorithmTest.java
+++ src/test/org/insa/algo/shortestpath/AStarAlgorithmTest.java
@@ -11,9 +11,4 @@ public class AStarAlgorithmTest extends ShortestPathAlgorithmTest {
 	public ShortestPathAlgorithm instanciateAlgorithm(ShortestPathData shortestPathData) throws NodeOutOfGraphException{
 		return new AStarAlgorithm(shortestPathData);
 	}
-
-	@Override
-	public void simpleGraphWithOraclePathTest(int from, int to) {
-		Assume.assumeTrue("Test graph not relevant for AStar Algorithm\n(distance between nodes can be inferior to their 'geographic' distance), thus the heuristic is an overestimation",false);
-	}
 }
