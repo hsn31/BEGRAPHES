2412f7b8f4b35fa78f6a865b369028e9e34ae0e7
M. Decaestecker Brice <decaeste@insa-08185>
2019-05-21 13:51:40 +0200

    Constructor for CarpoolingData

--- src/main/org/insa/algo/shortestpath/LabelStar.java
+++ src/main/org/insa/algo/shortestpath/LabelStar.java
@@ -3,6 +3,8 @@ package org.insa.algo.shortestpath;
 import org.insa.graph.Node;
 import org.insa.graph.Point; //Class representing a point (position) on Earth.
 import org.insa.algo.AbstractInputData;
+import org.insa.algo.carpooling.CarPoolingAlgorithm;
+import org.insa.algo.carpooling.CarPoolingData;
 
 public class LabelStar extends Label implements Comparable<Label> {
 	private double distToDest;
@@ -10,15 +12,37 @@ public class LabelStar extends Label implements Comparable<Label> {
 		super(noeud,cost);
 		if (data.getMode() == AbstractInputData.Mode.TIME) {
 			int speed = data.getMaximumSpeed();
-			if(speed<0) {
-				speed=150; //MAXIMUM
+			if(speed<=0) {
+				
+				speed=data.getGraph().getGraphInformation().getMaximumSpeed();
 			}
-			this.distToDest = Point.distance(noeud.getPoint(),data.getDestination().getPoint())/(speed);
+			else {
+				speed=Integer.min(speed, data.getGraph().getGraphInformation().getMaximumSpeed());
+			}
+			this.distToDest = Point.distance(noeud.getPoint(),data.getDestination().getPoint())/(speed/3.6);
 		}
 		else {
 			this.distToDest=Point.distance(noeud.getPoint(), data.getDestination().getPoint());
 		}
 	}
+	public LabelStar(Node noeud,double cost,CarPoolingData data) {
+		super(noeud,cost);
+		if (data.getMode() == AbstractInputData.Mode.TIME) {
+			int speed = data.getMaximumSpeed();
+			if(speed<=0) {
+				
+				speed=data.getGraph().getGraphInformation().getMaximumSpeed();
+			}
+			else {
+				speed=Integer.min(speed, data.getGraph().getGraphInformation().getMaximumSpeed());
+			}
+			this.distToDest = Point.distance(noeud.getPoint(),data.getDestination().getPoint())/(speed/3.6);
+		}
+		else {
+			this.distToDest=Point.distance(noeud.getPoint(), data.getDestination().getPoint());
+		}
+	}
+
 	public double getDistToDest(){
 		return this.distToDest;
 	}
